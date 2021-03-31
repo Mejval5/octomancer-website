fc865fc5e3031bb92225c51c7e558b0c9fe39bc3
Wood <necesal.daniel@gmail.com>
2021-03-31 15:00:42 +0200

    Added social media to contacts page

--- src/components/octo/contactUs.vue
+++ src/components/octo/contactUs.vue
@@ -1,7 +1,20 @@
 <template>
-  <base-contact-form
-    ref="contactForm"
-  />
+  <v-row>
+    <v-col
+      cols="12"
+      md="6"
+    >
+      <base-business-info
+        title="FOLLOW US ON SOCIAL MEDIA"
+      />
+    </v-col>
+    <v-col
+      cols="12"
+      md="6"
+    >
+      <base-contact-form ref="contactForm" />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
